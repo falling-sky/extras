fef2074f91974f93b457f5d5786eb9b99a23d6cc
Jason Fesler <jfesler@guest.gigo.com>
2015-02-13 23:06:45 -0800

    WIP

---
+++ mtu1280.c
@@ -0,0 +1,551 @@
+/*  Copyright (C) 2011-2013  P.D. Buchan (pdbuchan@yahoo.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Send a "cooked" IPv6 ICMP packet via raw socket.
+// Need to specify destination MAC address.
+// Values set for echo request packet, includes some ICMP data.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>           // close()
+#include <string.h>           // strcpy, memset(), and memcpy()
+
+#include <netdb.h>            // struct addrinfo
+#include <sys/types.h>        // needed for socket(), uint8_t, uint16_t
+#include <sys/socket.h>       // needed for socket()
+#include <netinet/in.h>       // IPPROTO_RAW, IPPROTO_ICMPV6, INET6_ADDRSTRLEN
+#include <netinet/ip.h>       // IP_MAXPACKET (which is 65535)
+#include <netinet/ip6.h>      // struct ip6_hdr
+#include <netinet/icmp6.h>    // struct icmp6_hdr and ICMP6_ECHO_REQUEST
+#include <arpa/inet.h>        // inet_pton() and inet_ntop()
+#include <sys/ioctl.h>        // macro ioctl is defined
+#include <bits/ioctls.h>      // defines values for argument "request" of ioctl.
+#include <net/if.h>           // struct ifreq
+#include <linux/if_ether.h>   // ETH_P_IP = 0x0800, ETH_P_IPV6 = 0x86DD
+#include <linux/if_packet.h>  // struct sockaddr_ll (see man 7 packet)
+#include <net/ethernet.h>
+
+#include <errno.h>            // errno, perror()
+
+// Define some constants
+#define IP6_HDRLEN 40         // IPv6 header length
+#define ICMP_HDRLEN 8         // ICMP header length for echo request, excludes data
+
+
+// netfilter
+#include <linux/netfilter.h>            /* for NF_ACCEPT */
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+
+
+// Function prototypes
+uint16_t checksum (uint16_t *addr, int len);
+uint16_t icmp6_checksum (struct ip6_hdr, struct icmp6_hdr, uint8_t *, int);
+char *allocate_strmem (int);
+uint8_t *allocate_ustrmem (int);
+
+
+static u_int32_t block_packet (struct nfq_data *tb)
+{
+
+}
+
+int
+mtu1280_main (int argc, char **argv)
+{
+  int i, status, datalen, frame_length, sd, bytes;
+  char *interface, *target, *src_ip, *dst_ip;
+  struct ip6_hdr iphdr;
+  struct icmp6_hdr icmphdr;
+  uint8_t *data, *src_mac, *dst_mac, *ether_frame;
+  struct addrinfo hints, *res;
+  struct sockaddr_in6 *ipv6;
+  struct sockaddr_ll device;
+  struct ifreq ifr;
+  void *tmp;
+  
+  
+  
+
+  // Allocate memory for various arrays.
+  src_mac = allocate_ustrmem (6);
+  dst_mac = allocate_ustrmem (6);
+  data = allocate_ustrmem (IP_MAXPACKET);
+  ether_frame = allocate_ustrmem (IP_MAXPACKET);
+  interface = allocate_strmem (40);
+  target = allocate_strmem (INET6_ADDRSTRLEN);
+  src_ip = allocate_strmem (INET6_ADDRSTRLEN);
+  dst_ip = allocate_strmem (INET6_ADDRSTRLEN);
+
+  // Interface to send packet through.
+  strcpy (interface, "eth0");
+
+  // Submit request for a socket descriptor to look up interface.
+  if ((sd = socket (AF_INET6, SOCK_RAW, IPPROTO_RAW)) < 0) {
+    perror ("socket() failed to get socket descriptor for using ioctl() ");
+    exit (EXIT_FAILURE);
+  }
+
+  // Use ioctl() to look up interface name and get its MAC address.
+  memset (&ifr, 0, sizeof (ifr));
+  snprintf (ifr.ifr_name, sizeof (ifr.ifr_name), "%s", interface);
+  if (ioctl (sd, SIOCGIFHWADDR, &ifr) < 0) {
+    perror ("ioctl() failed to get source MAC address ");
+    return (EXIT_FAILURE);
+  }
+  close (sd);
+
+  // Copy source MAC address
+  memcpy (src_mac, ifr.ifr_hwaddr.sa_data, 6 * sizeof (uint8_t));
+
+  // Report source MAC address to stdout.
+  printf ("MAC address for interface %s is ", interface);
+  for (i=0; i<5; i++) {
+    printf ("%02x:", src_mac[i]);
+  }
+  printf ("%02x\n", src_mac[5]);
+
+  // Find interface index from interface name and store index in
+  // struct sockaddr_ll device, which will be used as an argument of sendto().
+  memset (&device, 0, sizeof (device));
+  if ((device.sll_ifindex = if_nametoindex (interface)) == 0) {
+    perror ("if_nametoindex() failed to obtain interface index ");
+    exit (EXIT_FAILURE);
+  }
+  printf ("Index for interface %s is %i\n", interface, device.sll_ifindex);
+
+  // Set destination MAC address: you need to fill this out
+  dst_mac[0] = 0xff;
+  dst_mac[1] = 0xff;
+  dst_mac[2] = 0xff;
+  dst_mac[3] = 0xff;
+  dst_mac[4] = 0xff;
+  dst_mac[5] = 0xff;
+
+  // Source IPv6 address: you need to fill this out
+  strcpy (src_ip, "2001:db8::214:51ff:fe2f:1556");
+
+  // Destination URL or IPv6 address: you need to fill this out
+  strcpy (target, "ipv6.google.com");
+
+  // Fill out hints for getaddrinfo().
+  memset (&hints, 0, sizeof (hints));
+  hints.ai_family = AF_INET6;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = hints.ai_flags | AI_CANONNAME;
+
+  // Resolve target using getaddrinfo().
+  if ((status = getaddrinfo (target, NULL, &hints, &res)) != 0) {
+    fprintf (stderr, "getaddrinfo() failed %s\n", gai_strerror (status));
+    exit (EXIT_FAILURE);
+  }
+  ipv6 = (struct sockaddr_in6 *) res->ai_addr;
+  tmp = &(ipv6->sin6_addr);
+  if (inet_ntop (AF_INET6, tmp, dst_ip, INET6_ADDRSTRLEN) == NULL) {
+    status = errno;
+    fprintf (stderr, "inet_ntop() failed.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+  freeaddrinfo (res);
+
+  // Fill out sockaddr_ll
+  device.sll_family = AF_PACKET;
+  device.sll_protocol = htons (ETH_P_IP);
+  memcpy (device.sll_addr, dst_mac, 6 * sizeof (uint8_t));
+  device.sll_halen = 6;
+
+  // ICMP data
+  datalen = 4;
+  data[0] = 'T';
+  data[1] = 'e';
+  data[2] = 's';
+  data[3] = 't';
+
+  // IPv6 header
+
+  // IPv6 version (4 bits), Traffic class (8 bits), Flow label (20 bits)
+  iphdr.ip6_flow = htonl ((6 << 28) | (0 << 20) | 0);
+
+  // Payload length (16 bits): ICMP header + ICMP data
+  iphdr.ip6_plen = htons (ICMP_HDRLEN + datalen);
+
+  // Next header (8 bits): 58 for ICMP
+  iphdr.ip6_nxt = IPPROTO_ICMPV6;
+
+  // Hop limit (8 bits): default to maximum value
+  iphdr.ip6_hops = 255;
+
+  // Source IPv6 address (128 bits)
+  if ((status = inet_pton (AF_INET6, src_ip, &(iphdr.ip6_src))) != 1) {
+    fprintf (stderr, "inet_pton() failed.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+
+  // Destination IPv6 address (128 bits)
+  if ((status = inet_pton (AF_INET6, dst_ip, &(iphdr.ip6_dst))) != 1) {
+    fprintf (stderr, "inet_pton() failed.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+
+  // ICMP header
+
+  // Message Type (8 bits): echo request
+  icmphdr.icmp6_type = ICMP6_ECHO_REQUEST;
+
+  // Message Code (8 bits): echo request
+  icmphdr.icmp6_code = 0;
+
+  // Identifier (16 bits): usually pid of sending process - pick a number
+  icmphdr.icmp6_id = htons (1000);
+
+  // Sequence Number (16 bits): starts at 0
+  icmphdr.icmp6_seq = htons (0);
+
+  // ICMP header checksum (16 bits): set to 0 when calculating checksum
+  icmphdr.icmp6_cksum = 0;
+  icmphdr.icmp6_cksum = icmp6_checksum (iphdr, icmphdr, data, datalen);
+
+  // Fill out ethernet frame header.
+
+  // Ethernet frame length = ethernet data (IP header + ICMP header + ICMP data)
+  frame_length = IP6_HDRLEN + ICMP_HDRLEN + datalen;
+
+  // IPv6 header
+  memcpy (ether_frame, &iphdr, IP6_HDRLEN * sizeof (uint8_t));
+
+  // ICMP header
+  memcpy (ether_frame + IP6_HDRLEN, &icmphdr, ICMP_HDRLEN * sizeof (uint8_t));
+
+  // ICMP data
+  memcpy (ether_frame + IP6_HDRLEN + ICMP_HDRLEN, data, datalen * sizeof (uint8_t));
+
+  // Open raw socket descriptor.
+  if ((sd = socket (PF_PACKET, SOCK_DGRAM, htons (ETH_P_ALL))) < 0) {
+    perror ("socket() failed ");
+    exit (EXIT_FAILURE);
+  }
+
+  // Send ethernet frame to socket.
+  if ((bytes = sendto (sd, ether_frame, frame_length, 0, (struct sockaddr *) &device, sizeof (device))) <= 0) {
+    perror ("sendto() failed");
+    exit (EXIT_FAILURE);
+  }
+
+  close (sd);
+
+  // Free allocated memory.
+  free (src_mac);
+  free (dst_mac);
+  free (data);
+  free (ether_frame);
+  free (interface);
+  free (target);
+  free (src_ip);
+  free (dst_ip);
+
+  return (EXIT_SUCCESS);
+}
+
+// Checksum function
+uint16_t
+checksum (uint16_t *addr, int len)
+{
+  int nleft = len;
+  int sum = 0;
+  uint16_t *w = addr;
+  uint16_t answer = 0;
+
+  while (nleft > 1) {
+    sum += *w++;
+    nleft -= sizeof (uint16_t);
+  }
+
+  if (nleft == 1) {
+    *(uint8_t *) (&answer) = *(uint8_t *) w;
+    sum += answer;
+  }
+
+  sum = (sum >> 16) + (sum & 0xFFFF);
+  sum += (sum >> 16);
+  answer = ~sum;
+  return (answer);
+}
+
+// Build IPv6 ICMP pseudo-header and call checksum function (Section 8.1 of RFC 2460).
+uint16_t
+icmp6_checksum (struct ip6_hdr iphdr, struct icmp6_hdr icmp6hdr, uint8_t *payload, int payloadlen)
+{
+  char buf[IP_MAXPACKET];
+  char *ptr;
+  int chksumlen = 0;
+  int i;
+
+  ptr = &buf[0];  // ptr points to beginning of buffer buf
+
+  // Copy source IP address into buf (128 bits)
+  memcpy (ptr, &iphdr.ip6_src.s6_addr, sizeof (iphdr.ip6_src.s6_addr));
+  ptr += sizeof (iphdr.ip6_src);
+  chksumlen += sizeof (iphdr.ip6_src);
+
+  // Copy destination IP address into buf (128 bits)
+  memcpy (ptr, &iphdr.ip6_dst.s6_addr, sizeof (iphdr.ip6_dst.s6_addr));
+  ptr += sizeof (iphdr.ip6_dst.s6_addr);
+  chksumlen += sizeof (iphdr.ip6_dst.s6_addr);
+
+  // Copy Upper Layer Packet length into buf (32 bits).
+  // Should not be greater than 65535 (i.e., 2 bytes).
+  *ptr = 0; ptr++;
+  *ptr = 0; ptr++;
+  *ptr = (ICMP_HDRLEN + payloadlen) / 256;
+  ptr++;
+  *ptr = (ICMP_HDRLEN + payloadlen) % 256;
+  ptr++;
+  chksumlen += 4;
+
+  // Copy zero field to buf (24 bits)
+  *ptr = 0; ptr++;
+  *ptr = 0; ptr++;
+  *ptr = 0; ptr++;
+  chksumlen += 3;
+
+  // Copy next header field to buf (8 bits)
+  memcpy (ptr, &iphdr.ip6_nxt, sizeof (iphdr.ip6_nxt));
+  ptr += sizeof (iphdr.ip6_nxt);
+  chksumlen += sizeof (iphdr.ip6_nxt);
+
+  // Copy ICMPv6 type to buf (8 bits)
+  memcpy (ptr, &icmp6hdr.icmp6_type, sizeof (icmp6hdr.icmp6_type));
+  ptr += sizeof (icmp6hdr.icmp6_type);
+  chksumlen += sizeof (icmp6hdr.icmp6_type);
+
+  // Copy ICMPv6 code to buf (8 bits)
+  memcpy (ptr, &icmp6hdr.icmp6_code, sizeof (icmp6hdr.icmp6_code));
+  ptr += sizeof (icmp6hdr.icmp6_code);
+  chksumlen += sizeof (icmp6hdr.icmp6_code);
+
+  // Copy ICMPv6 ID to buf (16 bits)
+  memcpy (ptr, &icmp6hdr.icmp6_id, sizeof (icmp6hdr.icmp6_id));
+  ptr += sizeof (icmp6hdr.icmp6_id);
+  chksumlen += sizeof (icmp6hdr.icmp6_id);
+
+  // Copy ICMPv6 sequence number to buff (16 bits)
+  memcpy (ptr, &icmp6hdr.icmp6_seq, sizeof (icmp6hdr.icmp6_seq));
+  ptr += sizeof (icmp6hdr.icmp6_seq);
+  chksumlen += sizeof (icmp6hdr.icmp6_seq);
+
+  // Copy ICMPv6 checksum to buf (16 bits)
+  // Zero, since we don't know it yet.
+  *ptr = 0; ptr++;
+  *ptr = 0; ptr++;
+  chksumlen += 2;
+
+  // Copy ICMPv6 payload to buf
+  memcpy (ptr, payload, payloadlen * sizeof (uint8_t));
+  ptr += payloadlen;
+  chksumlen += payloadlen;
+
+  // Pad to the next 16-bit boundary
+  for (i=0; i<payloadlen%2; i++, ptr++) {
+    *ptr = 0;
+    ptr += 1;
+    chksumlen += 1;
+  }
+
+  return checksum ((uint16_t *) buf, chksumlen);
+}
+
+// Allocate memory for an array of chars.
+char *
+allocate_strmem (int len)
+{
+  void *tmp;
+
+  if (len <= 0) {
+    fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_strmem().\n", len);
+    exit (EXIT_FAILURE);
+  }
+
+  tmp = (char *) malloc (len * sizeof (char));
+  if (tmp != NULL) {
+    memset (tmp, 0, len * sizeof (char));
+    return (tmp);
+  } else {
+    fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_strmem().\n");
+    exit (EXIT_FAILURE);
+  }
+}
+
+// Allocate memory for an array of unsigned chars.
+uint8_t *
+allocate_ustrmem (int len)
+{
+  void *tmp;
+
+  if (len <= 0) {
+    fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_ustrmem().\n", len);
+    exit (EXIT_FAILURE);
+  }
+
+  tmp = (uint8_t *) malloc (len * sizeof (uint8_t));
+  if (tmp != NULL) {
+    memset (tmp, 0, len * sizeof (uint8_t));
+    return (tmp);
+  } else {
+    fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_ustrmem().\n");
+    exit (EXIT_FAILURE);
+  }
+}
+
+/* returns packet id */
+static u_int32_t print_pkt (struct nfq_data *tb)
+{
+        int id = 0;
+        struct nfqnl_msg_packet_hdr *ph;
+        struct nfqnl_msg_packet_hw *hwph;
+        u_int32_t mark,ifi; 
+        int ret;
+        unsigned char *data;
+
+        ph = nfq_get_msg_packet_hdr(tb);
+        if (ph) {
+                id = ntohl(ph->packet_id);
+                printf("hw_protocol=0x%04x hook=%u id=%u ",
+                        ntohs(ph->hw_protocol), ph->hook, id);
+        }
+
+        hwph = nfq_get_packet_hw(tb);
+        if (hwph) {
+                int i, hlen = ntohs(hwph->hw_addrlen);
+
+                printf("hw_src_addr=");
+                for (i = 0; i < hlen-1; i++)
+                        printf("%02x:", hwph->hw_addr[i]);
+                printf("%02x ", hwph->hw_addr[hlen-1]);
+        }
+
+        mark = nfq_get_nfmark(tb);
+        if (mark)
+                printf("mark=%u ", mark);
+
+        ifi = nfq_get_indev(tb);
+        if (ifi)
+                printf("indev=%u ", ifi);
+
+        ifi = nfq_get_outdev(tb);
+        if (ifi)
+                printf("outdev=%u ", ifi);
+        ifi = nfq_get_physindev(tb);
+        if (ifi)
+                printf("physindev=%u ", ifi);
+
+        ifi = nfq_get_physoutdev(tb);
+        if (ifi)
+                printf("physoutdev=%u ", ifi);
+
+        ret = nfq_get_payload(tb, &data);
+        if (ret >= 0)
+                printf("payload_len=%d ", ret);
+
+        fputc('\n', stdout);
+
+        return id;
+}
+        
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+              struct nfq_data *nfa, void *data)
+{
+        u_int32_t id = print_pkt(nfa);
+        printf("entering callback\n");
+        return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+}
+
+
+
+int main(int argc, char **argv)
+{
+        struct nfq_handle *h;
+        struct nfq_q_handle *qh;
+        struct nfnl_handle *nh;
+        int fd;
+        int rv;
+        unsigned int queue;
+        char *interface;
+        char buf[4096] __attribute__ ((aligned));
+
+  if (argc != 3) {
+    fprintf(stderr,"usage: a.out interface netgroup\n");
+    exit(1);
+  }
+  interface = argv[1];
+  queue = strtol(argv[2],NULL,10);
+  
+
+        printf("opening library handle\n");
+        h = nfq_open();
+        if (!h) {
+                fprintf(stderr, "error during nfq_open()\n");
+                exit(1);
+        }
+
+        printf("unbinding existing nf_queue handler for AF_INET6 (if any)\n");
+        if (nfq_unbind_pf(h, AF_INET6) < 0) {
+                fprintf(stderr, "error during nfq_unbind_pf()\n");
+                exit(1);
+        }
+
+        printf("binding nfnetlink_queue as nf_queue handler for AF_INET6\n");
+        if (nfq_bind_pf(h, AF_INET6) < 0) {
+                fprintf(stderr, "error during nfq_bind_pf()\n");
+                exit(1);
+        }
+
+        printf("binding this socket to queue '%u'\n",queue);
+        qh = nfq_create_queue(h,  queue, &cb, NULL);
+        if (!qh) {
+                fprintf(stderr, "error during nfq_create_queue()\n");
+                exit(1);
+        }
+
+        printf("setting copy_packet mode\n");
+        if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+                fprintf(stderr, "can't set packet_copy mode\n");
+                exit(1);
+        }
+
+        fd = nfq_fd(h);
+
+        while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
+                printf("pkt received\n");
+                nfq_handle_packet(h, buf, rv);
+        }
+
+        printf("unbinding from queue 0\n");
+        nfq_destroy_queue(qh);
+
+#ifdef INSANE
+        /* normally, applications SHOULD NOT issue this command, since
+         * it detaches other programs/sockets from AF_INET6, too ! */
+        printf("unbinding from AF_INET6\n");
+        nfq_unbind_pf(h, AF_INET6);
+#endif
+
+        printf("closing library handle\n");
+        nfq_close(h);
+
+        exit(0);
+}
+
